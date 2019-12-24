5f06af63c58401db96f2f56bc6f079d9f3c98a66
wuyumk7 <hymeldon@gmail.com>
2019-12-24 01:23:33 +0000

    feat(auth): add email check for new users
    The site only accepts users whose email exists in the
    site/information/users collection.

--- functions/index.js
+++ functions/index.js
@@ -13,17 +13,30 @@ let db = admin.firestore();
 
 exports.addUserRecords = functions.auth.user().onCreate(
   (user) => {
-    db.collection('individual').doc(user.uid).set({
-      'email': user.email,
-      'name': user.displayName,
-      'avatar': user.photoURL,
-      'groups': []
-    });
+    return db.collection('site').doc('information')
+	     .collection('users').doc(user.email)
+	     .get().then(doc=> {
+	       if (doc.exists) {
+                 db.collection('individual').doc(user.uid).set({
+                   'email': user.email,
+                   'name': user.displayName,
+                   'avatar': user.photoURL,
+                   'groups': []
+                 });
+	       }
+	       return true; 
+	     }).catch(err=>console.log(`Error: ${err}`))
   }
 );
 
 exports.deleteUserRecords = functions.auth.user().onDelete(
   (user) => {
     db.collection('individual').doc(user.uid).delete();
+    // TODO: change site/information/users/user.uid as well 
   }
 );
+
+exports.readData = functions.https.onRequest((req, resp) => {
+  db.collection('site').doc('information').collection('users').doc('hymeldon@gmail.com').get()
+	.then((doc)=>{return console.log(doc.data())}).catch(err=>console.log(err));
+});
