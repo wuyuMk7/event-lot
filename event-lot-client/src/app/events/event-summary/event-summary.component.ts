2fb90dfdb2a1409e1b9e7f64a01dcf6a646facfb
wuyumk7 <hymeldon@gmail.com>
2020-01-05 02:19:54 +0000

    feat(events): add checklist to event summary page

--- event-lot-client/src/app/events/event-summary/event-summary.component.ts
+++ event-lot-client/src/app/events/event-summary/event-summary.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, OnChanges,
+  Input, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 import { Event } from '../../_models/event';
 
@@ -9,9 +12,19 @@ import { Event } from '../../_models/event';
 })
 export class EventSummaryComponent implements OnInit {
   @Input() eventFormData: any;
+  @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
+
+  displayedColumns = ['index', 'content', 'status'];
+  dataSource: MatTableDataSource<any>;
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  ngOnChanges() {
+    if (this.eventFormData && this.eventFormData.checklist) {
+      this.dataSource = new MatTableDataSource<any>(this.eventFormData.checklist.checklist);
+      this.dataSource.paginator = this.paginator;
+    }
   }
 }
