0fcacf66cf0b589518356c5fafef77b1c229c0ae
wuyumk7 <hymeldon@gmail.com>
2020-01-17 04:09:31 +0000

    feat(events): add EventAll component to Events module and router

--- event-lot-client/src/app/events/events-routing.module.ts
+++ event-lot-client/src/app/events/events-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { EventAllComponent } from './event-all/event-all.component';
 import { AddEventComponent } from './add-event/add-event.component';
 import { EventDetailComponent } from './event-detail/event-detail.component';
 import { EventModificationComponent } from './event-modification/event-modification.component';
 
 const routes: Routes = [
+  { path: '', component: EventAllComponent },
   { path: 'add', component: AddEventComponent },
   { path: 'detail/:id', component: EventDetailComponent },
   { path: 'update/:id', component: EventModificationComponent },
